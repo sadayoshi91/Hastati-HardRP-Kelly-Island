0831b2d580cfcd3b47ffa7290f8c2abdeafb8cdc
sadayoshi91 <theprobb@gmail.com>
2016-10-11 18:56:26 +0200

    Update Config_MarketItems.hpp

--- Kellys.kelleysisland/Config_MarketItems.hpp
+++ Kellys.kelleysisland/Config_MarketItems.hpp
@@ -11,82 +11,82 @@ class MarketItems {
 	
     class oil_processed {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 450;		//weight = 7;
+		minPrice = 90;
+		consumption = 40;
+		maxQuantity = 2000;
     };
 
     class copper_refined {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
+		maxPrice = 550;		//weight = 3;
+		minPrice = 110;
 		consumption = 50;
-		maxQuantity = 1000;
+		maxQuantity = 2500;
     };
 
     class iron_refined {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
+		maxPrice = 650; 		//weight = 3;
+		minPrice = 130;
 		consumption = 50;
-		maxQuantity = 1000;
+		maxQuantity = 2500;
     };
 
     class salt_refined {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 350;		//weight = 1;
+		minPrice = 70;
+		consumption = 60;
+		maxQuantity = 3000;
     };
 
     class diamond_cut {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
+		maxPrice = 3500;		//weight = 2;
+		minPrice = 700;
+		consumption = 20;
 		maxQuantity = 1000;
     };
 
     class cement {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 480;		//weight = 5;
+		minPrice = 96;
+		consumption = 40;
+		maxQuantity = 2000;
     };
 
     class heroin_processed {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 3500;		//weight = 4;
+		minPrice = 700;	
+		consumption = 10;
+		maxQuantity = 500;
     };
 	
     class marijuana {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 800;		//weight = 3;
+		minPrice = 160;
+		consumption = 18;
+		maxQuantity = 900;
     };
 
     class cocaine_processed {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 7000; 		//weight = 4;
+		minPrice = 1400;
+		consumption = 10;
+		maxQuantity = 500;
     };
 
     class glass {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 350;	  		//weight = 1;
+		minPrice = 70;
+		consumption = 40;
+		maxQuantity = 2000;
         icon = "";
     };
 	
@@ -99,85 +99,85 @@ class MarketItems {
     //Mined Items
     class oil_unprocessed {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 225;		//weight = 7;
+		minPrice = 45;
+		consumption = 40;
+		maxQuantity = 2000;
     };
 
     class copper_unrefined {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
+		maxPrice = 275;		//weight = 4;
+		minPrice = 55;
 		consumption = 50;
-		maxQuantity = 1000;
+		maxQuantity = 2500;
     };
 
     class iron_unrefined {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
+		maxPrice = 325;		//weight = 5;
+		minPrice = 65;
 		consumption = 50;
-		maxQuantity = 1000;
+		maxQuantity = 2500;
     };
 
     class salt_unrefined {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 175;		//weight = 3;
+		minPrice = 35;
+		consumption = 60;
+		maxQuantity = 3000;
     };
 
     class sand {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 175;		//weight = 3;
+		minPrice = 35;
+		consumption = 40;
+		maxQuantity = 2000;
     };
 
     class diamond_uncut {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
+		maxPrice = 1750;		//weight = 4;
+		minPrice = 350;
+		consumption = 20;
 		maxQuantity = 1000;
     };
 
     class rock {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 240;		//weight = 6;
+		minPrice = 48;
+		consumption = 40;
+		maxQuantity = 2000;
         icon = "";
     };
 
     //Drugs
     class heroin_unprocessed {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 1750; 		//weight = 6;
+		minPrice = 875;
+		consumption = 10;
+		maxQuantity = 500;
     };
 
     class cannabis {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 400; 		//weight = 4;
+		minPrice = 200;
+		consumption = 18;
+		maxQuantity = 900;
     };
 
 	
     class cocaine_unprocessed {
 		startPrice = 200;
-		maxPrice = 400;
-		minPrice = 0;
-		consumption = 50;
-		maxQuantity = 1000;
+		maxPrice = 3500;	 		//weight = 6;
+		minPrice = 700;
+		consumption = 10;
+		maxQuantity = 500;
     };
 
 
